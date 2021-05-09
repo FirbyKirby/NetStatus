6051c3cc4b9d6365fa60667f1dc8de6383b0089c
Asa Kirby <asakirby@gmail.com>
2021-05-08 21:27:48 -0500

    Update offline.map.ts
    Added date if connection lost longer then a day.

--- src/controllers/offline.map.ts
+++ src/controllers/offline.map.ts
@@ -4,12 +4,24 @@ import { IGlobalState } from "../state/interface";
 
 
 const mapStateToProps = (state: IGlobalState): IOfflineProps => {
-    const timeFormat = new Intl.DateTimeFormat('en', { hour: 'numeric', minute: 'numeric' });
-    return {
-        title: "Connection Lost!",
-        subtitle: "Last online at " + timeFormat.format(state.OnlineStatus.dateWasLastOnline),
-        iconName: "plug",
-    };
+
+    const timeFormat = new Intl.DateTimeFormat('default', { hour: 'numeric', minute: 'numeric' });  //Time format if lost connection within a day.
+    const dateFormat = new Intl.DateTimeFormat('default', { year: 'numeric', month: 'numeric', day: 'numeric', hour: 'numeric', minute: 'numeric' }); //Time with date format if lost connection longer then a day.
+    const now = new Date(); //Get what time it is right now.
+
+    if(now.getDate() === state.OnlineStatus.dateWasLastOnline.getDate() && now.getMonth() === state.OnlineStatus.dateWasLastOnline.getMonth() && now.getFullYear() === state.OnlineStatus.dateWasLastOnline.getFullYear()){
+        return {
+            title: "Connection Lost!",
+            subtitle: "Last online at " + timeFormat.format(state.OnlineStatus.dateWasLastOnline),
+            iconName: "plug",
+        };  //If connection was lost within a day, just show the time.
+    } else {
+        return {
+            title: "Connection Lost!",
+            subtitle: "Last online at " + dateFormat.format(state.OnlineStatus.dateWasLastOnline),
+            iconName: "plug",
+        };  //If connection ws lost more then a day ago, show the date as well.
+    }
 }
 
 export default mapStateToProps;
